a9fce8c4698d66206b56efd13d37ce61456a3001
TylerHayward <Tylerhayward136@gmail.com>
2026-02-05 23:19:33 -0500

    part of lab 3

--- Labs/COMP3987-W26/Assets/Scripts/PlayerInput.cs
+++ Labs/COMP3987-W26/Assets/Scripts/PlayerInput.cs
@@ -14,6 +14,7 @@ public class PlayerInput : MonoBehaviour
     private Vector3 velocity;
     [SerializeField] private float rotationSpeed = 4.0f;
     [SerializeField] private float mouseSensY = 5.0f;
+    private float camXRotation;
     [SerializeField, Self] private CharacterController controller;
     [SerializeField, Child] private Camera cam;
     private void OnValidate()
@@ -25,11 +26,9 @@ public class PlayerInput : MonoBehaviour
     {
         move = InputSystem.actions.FindAction("Player/Move");
         look = InputSystem.actions.FindAction("Player/Look");
-        controller = GetComponent<CharacterController>();
-        if (controller == null)
-        {
-            controller = gameObject.AddComponent<CharacterController>();
-        }
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
     }
 
     
@@ -50,8 +49,8 @@ public class PlayerInput : MonoBehaviour
         transform.Rotate(Vector3.up, readLook.x * rotationSpeed * Time.deltaTime);
 
         // Rotate the Camera
-        mouseSensY = mouseSensY * readLook.y;
-        mouseSensY = Mathf.Clamp(mouseSensY, -90f, 90f);
-        cam.gameObject.transform.localRotation = Quaternion.Euler(mouseSensY, 0, 0);
+        camXRotation += mouseSensY * readLook.y * Time.deltaTime * -1;
+        camXRotation = Mathf.Clamp(camXRotation, -90f, 90f);
+        cam.gameObject.transform.localRotation = Quaternion.Euler(camXRotation, 0, 0);
     }
 }
